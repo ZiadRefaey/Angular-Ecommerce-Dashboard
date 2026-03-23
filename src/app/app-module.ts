af90238c6ebc1fcf05c5c456197fd9fd21ae7987
Ziad Refaey <ziad.refaey1@gmail.com>
2026-03-23 15:13:26 +0200

    build(deps): install primeng

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -1,25 +1,30 @@
-import { NgModule, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import {
+  NgModule,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
+
 import { SideBar } from './shared/components/side-bar/side-bar';
 import { Layout } from './shared/components/layout/layout';
 
 @NgModule({
-  declarations: [
-    App,
-    SideBar,
-    Layout
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [App, SideBar, Layout],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+      },
+    }),
   ],
-  bootstrap: [App]
+  bootstrap: [App],
 })
-export class AppModule { }
+export class AppModule {}
