b45a5724077f561a54a093f605026648bf1ea941
Ziad Refaey <ziad.refaey1@gmail.com>
2026-03-26 20:38:25 +0200

    feat(routes): adding modules for features then lazy loading their routes

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,10 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full',
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () =>
+      import('./features/dashboard/dashboard-module').then((m) => m.DashboardModule),
+  },
+  {
+    path: 'products',
+    loadChildren: () => import('./features/products/products-module').then((m) => m.ProductsModule),
+  },
+  {
+    path: 'orders',
+    loadChildren: () => import('./features/orders/orders-module').then((m) => m.OrdersModule),
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./features/auth/auth-module').then((m) => m.AuthModule),
+  },
+
+  {
+    path: '**',
+    redirectTo: 'dashboard',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
